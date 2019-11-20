235df030424320a9246ce0cfcf06544ab35f9896
Maxim Topchu <maximtop@gmail.com>
2019-11-20 13:42:03 +0300

    VPN-28 turn off websocket debug

--- src/background/api/websocketApi.js
+++ src/background/api/websocketApi.js
@@ -11,7 +11,7 @@ class WebsocketApi {
         maxRetries: Infinity,
         maxEnqueuedMessages: Infinity,
         startClosed: false,
-        debug: true,
+        debug: false,
     };
 
     constructor(url) {
