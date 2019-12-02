288152b2f30d0047288cd28192bbe4f3b0e733d1
Maxim Topchu <maximtop@gmail.com>
2019-12-02 16:49:01 +0300

    VPN-47 throw error in compilation time if wrong browser specified

--- tasks/webpack.common.js
+++ tasks/webpack.common.js
@@ -72,9 +72,11 @@ const config = {
             if (process.env.BROWSER === 'firefox') {
                 // eslint-disable-next-line no-param-reassign
                 resource.request = resource.request.replace(/\.\/abstractProxyApi/, './firefox/proxyApi');
-            } else {
+            } else if (process.env.BROWSER === 'chrome' || process.env.BROWSER === 'edge') {
                 // eslint-disable-next-line no-param-reassign
                 resource.request = resource.request.replace(/\.\/abstractProxyApi/, './chrome/proxyApi');
+            } else {
+                throw new Error(`There is no proxy api for browser: ${process.env.BROWSER}`);
             }
         })),
         new CleanWebpackPlugin(cleanOptions),
