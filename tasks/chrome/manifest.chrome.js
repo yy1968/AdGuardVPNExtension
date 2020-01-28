7f33abb591da9a1194ab2238228feb9790bdc037
Maxim Topciu <maximtop@gmail.com>
2020-01-28 19:16:39 +0300

    VPN-114 add context menu to control vpn

--- tasks/chrome/manifest.chrome.js
+++ tasks/chrome/manifest.chrome.js
@@ -1,15 +1,4 @@
 module.exports = {
     minimum_chrome_version: '55.0',
     options_page: 'options.html',
-    permissions: [
-        'storage',
-        'unlimitedStorage',
-        'proxy',
-        'notifications',
-        'activeTab',
-        'management',
-        'webRequest',
-        'webRequestBlocking',
-        '<all_urls>',
-    ],
 };
