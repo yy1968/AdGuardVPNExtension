7585853cb6fdeae7be5d97faa56c682ab78fdee2
Maxim Topciu <maximtop@gmail.com>
2020-02-25 15:52:44 +0300

    AG-225 fix stats storage

--- src/background/connectivity/statsStorage/index.js
+++ src/background/connectivity/statsStorage/index.js
@@ -1,6 +1,6 @@
-import storage from '../../browserApi/storage';
+import browserApi from '../../browserApi';
 import StatsStorage from './StatsStorage';
 
-const statsStorage = new StatsStorage(storage);
+const statsStorage = new StatsStorage(browserApi.storage);
 
 export default statsStorage;
